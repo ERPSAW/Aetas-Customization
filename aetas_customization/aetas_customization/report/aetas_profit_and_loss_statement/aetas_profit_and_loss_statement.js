946612c1461c5aa26676c445b4b443402a2cddf8
l0gesh29 <logeshperiyasamy24@gmail.com>
2024-07-24 09:39:36 +0530

    fix: version filter issue

--- aetas_customization/aetas_customization/report/aetas_profit_and_loss_statement/aetas_profit_and_loss_statement.js
+++ aetas_customization/aetas_customization/report/aetas_profit_and_loss_statement/aetas_profit_and_loss_statement.js
@@ -2,28 +2,30 @@
 // For license information, please see license.txt
 
 
-frappe.query_reports["Aetas Profit and Loss Statement"] = $.extend({}, erpnext.financial_statements);
+frappe.require("assets/erpnext/js/financial_statements.js", function () {
+	frappe.query_reports["Aetas Profit and Loss Statement"] = $.extend({}, erpnext.financial_statements);
 
-erpnext.utils.add_dimensions("Aetas Profit and Loss Statement", 10);
+	erpnext.utils.add_dimensions("Aetas Profit and Loss Statement", 10);
 
-frappe.query_reports["Aetas Profit and Loss Statement"]["filters"].push({
-	fieldname: "selected_view",
-	label: __("Select View"),
-	fieldtype: "Select",
-	options: [
-		{ value: "Report", label: __("Report View") },
-		{ value: "Growth", label: __("Growth View") },
-		{ value: "Margin", label: __("Margin View") },
-	],
-	default: "Report",
-	reqd: 1,
-});
+	frappe.query_reports["Aetas Profit and Loss Statement"]["filters"].push({
+		fieldname: "selected_view",
+		label: __("Select View"),
+		fieldtype: "Select",
+		options: [
+			{ value: "Report", label: __("Report View") },
+			{ value: "Growth", label: __("Growth View") },
+			{ value: "Margin", label: __("Margin View") },
+		],
+		default: "Report",
+		reqd: 1,
+	});
 
-frappe.query_reports["Aetas Profit and Loss Statement"]["filters"].push({
-	fieldname: "accumulated_values",
-	label: __("Accumulated Values"),
-	fieldtype: "Check",
-	default: 1,
+	frappe.query_reports["Aetas Profit and Loss Statement"]["filters"].push({
+		fieldname: "include_default_book_entries",
+		label: __("Include Default Book Entries"),
+		fieldtype: "Check",
+		default: 1,
+	});
 });
 
 frappe.query_reports["Aetas Profit and Loss Statement"]["filters"].push({
