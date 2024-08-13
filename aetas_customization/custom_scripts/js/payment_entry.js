5ed71558db80f872b16498f9fec16e9aea73c784
tailorraj <tailorraj111@gmail.com>
2024-08-13 12:36:09 +0500

    changes in aetas advance receipt doctype

--- aetas_customization/custom_scripts/js/payment_entry.js
+++ aetas_customization/custom_scripts/js/payment_entry.js
@@ -1,8 +1,6 @@
 frappe.ui.form.on('Payment Entry', {
     refresh:function(frm){
-        console.log("refresh event")
         frm.set_query("custom_advance_payment_receipt",function(){
-            console.log("in function")
             return {
                 query: 'aetas_customization.aetas_customization.overrides.payment_entry.custom_query',
                 filters: {
