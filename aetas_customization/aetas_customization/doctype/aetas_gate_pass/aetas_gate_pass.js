3c91640a201d431e791d11789f31249ef6ed23ef
tailorraj <tailorraj111@gmail.com>
2025-04-10 18:18:07 +0530

    aetas gate pass document changes

--- aetas_customization/aetas_customization/doctype/aetas_gate_pass/aetas_gate_pass.js
+++ aetas_customization/aetas_customization/doctype/aetas_gate_pass/aetas_gate_pass.js
@@ -2,9 +2,43 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('AETAS Gate Pass', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if(frm.doc.docstatus === 1 && !frm.doc.is_received){ 
+			frm.add_custom_button("Received", function() {
+				let d = new frappe.ui.Dialog({
+					title: 'Receiver details',
+					fields: [
+						{
+							label: 'Receiver Name',
+							fieldname: 'receiver_name',
+							fieldtype: 'Data'
+						},
+						
+					],
+					size: 'small', // small, large, extra-large 
+					primary_action_label: 'Submit',
+					primary_action(values) {
+						if(!values.receiver_name){
+							frappe.msgprint(__("Please enter receiver name"));
+							return;
+						}
+						frm.set_value("received_by", values.receiver_name);
+						frm.set_value("is_received", 1);
+						frm.set_value("date_of_received", frappe.datetime.now_datetime());
+						frm.refresh_field("is_received","received_by","date_of_received");
+						if(frm.doc.docstatus === 1 ){
+							frm.save('Update');
+						}else{
+							frm.save()
+						}
+						d.hide();
+					}
+				});
+				
+				d.show();
+			});
+		}
+	},
 	sender_boutique:function(frm){
 		if(frm.doc.sender_boutique){
 			frappe.db.get_value("Boutique", frm.doc.sender_boutique, "boutique_location_display", function(r){
