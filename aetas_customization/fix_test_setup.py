87da63658c2a946c59682c97a536bd23f8e44785
Yemi Kudaisi <yemikudaisi@gmail.com>
2026-04-28 06:52:22 +0000

    removed static files

--- aetas_customization/fix_test_setup.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def fix_lead_source():
-    if not frappe.db.exists("Lead Source", "Others"):
-        frappe.get_doc({
-            "doctype": "Lead Source",
-            "source_name": "Others"
-        }).insert(ignore_permissions=True)
